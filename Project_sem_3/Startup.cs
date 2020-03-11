84342f0acaec6c146b467cd3edf0e281f25fd1a1
datnguyenvan11 <datnvth1807036@fpt.edu.vn>
2020-03-11 23:48:52 +0700

    firstcommit

---
+++ Project_sem_3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_sem_3.Startup))]
+namespace Project_sem_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
