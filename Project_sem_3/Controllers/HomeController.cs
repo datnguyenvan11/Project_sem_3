b9a5400554dadfdbaf8a19d4ab7467e72226b492
Anh Hoc Dot <anh@hocdot.com>
2020-03-12 14:20:52 +0700

    template

--- Project_sem_3/Controllers/HomeController.cs
+++ Project_sem_3/Controllers/HomeController.cs
@@ -12,19 +12,31 @@ namespace Project_sem_3.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            return View();
+        }
+        public ActionResult HouseInsurance()
+        {
+            return View();
+        }
+        public ActionResult MotorInsurance()
+        {
+            return View();
+        }
+        public ActionResult MedicalInsurance()
+        {
+            return View();
+        }
+        public ActionResult lifeInsurance()
+        {
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
     }
 }
