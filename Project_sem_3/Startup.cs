7f5feb4eb0b1c71bf49809faa96f8458e8d04aeb
datnguyenvan11 <datnvth1807036@fpt.edu.vn>
2020-03-12 13:16:04 +0700

    'model'

--- Project_sem_3/Startup.cs
+++ Project_sem_3/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Project_sem_3.App_Start;
+using Project_sem_3.Models;
 
 [assembly: OwinStartupAttribute(typeof(Project_sem_3.Startup))]
 namespace Project_sem_3
@@ -8,7 +12,16 @@ namespace Project_sem_3
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.CreatePerOwinContext(MyDb.Create);
+            app.CreatePerOwinContext<MyUserManager>(MyUserManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Accounts/Login"),
+
+            });
+
         }
     }
 }
