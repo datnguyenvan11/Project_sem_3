62b9d12411107efe5b0ec75229ce9c56128b08a8
Tuan Anh <anhntth1807058@fpt.edu.vn>
2020-03-13 15:30:26 +0700

    CRUD Insurance

--- Project_sem_3/Migrations/Configuration.cs
+++ Project_sem_3/Migrations/Configuration.cs
@@ -14,10 +14,17 @@ namespace Project_sem_3.Migrations
 
         protected override void Seed(Project_sem_3.Models.MyDb context)
         {
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Insurances.AddOrUpdate(x => x.Id,
+                new Models.Insurance() { Id = 1, Name = "HomeInsurance" },
+                new Models.Insurance() { Id = 2, Name = "LifeInsurance" },
+                new Models.Insurance() { Id = 3, Name = "MotorInsurance" },
+                new Models.Insurance() { Id = 4, Name = "MedicalInsurance" }
+                );
         }
     }
 }
