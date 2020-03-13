e6833092acb3d25385f8f4a8eca33099e7283c30
datnguyenvan11 <datnvth1807036@fpt.edu.vn>
2020-03-13 15:11:26 +0700

    'updatehouse'

--- Project_sem_3/Controllers/HomeInsuranceController.cs
+++ Project_sem_3/Controllers/HomeInsuranceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_sem_3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace Project_sem_3.Controllers
 {
     public class HomeInsuranceController : Controller
     {
+        private MyDb db = new MyDb();
+
         // GET: HomeInsurance
         public ActionResult Index()
         {
@@ -17,5 +20,55 @@ namespace Project_sem_3.Controllers
         {
             return View();
         }
+
+        public ActionResult CreateContract(int InsurancePackageId, string HouseType, string DurationHouse, string HouseOwner, string HouserAddress)
+        {
+
+            // load cart trong session.
+            var insurancepackage = db.InsurancePackages.Find(InsurancePackageId);
+
+            var contract = new Contract
+            {
+                TotalPrice = insurancepackage.Price,
+                CustomerId = 1,
+                InsuranceId = 2,
+                HouseInsurances = new List<HouseInsurance>()
+            };
+
+
+            var houseinsurance = new HouseInsurance()
+            {
+                InsurancePackageId = insurancepackage.Id,
+                HouseOwner = HouseOwner,
+                HouseType = HouseType,
+                HouserAddress = HouserAddress,
+                DurationHouse = DurationHouse,
+                ContractId = contract.Id,
+                Quantity = 1,
+                UnitPrice = insurancepackage.Price
+            };
+            contract.HouseInsurances.Add(houseinsurance);
+            db.Contracts.Add(contract);
+
+
+            db.SaveChanges();
+
+            // lưu vào database.
+            var transaction = db.Database.BeginTransaction();
+            try
+            {
+
+                transaction.Commit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                transaction.Rollback();
+            }
+            return Redirect("/MotorCart/ShowCart");
+
+
+        }
+
     }
 }
