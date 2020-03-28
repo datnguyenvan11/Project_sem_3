437f4cf254f34b5bf28a22f2669e4cca161bdbea
Tuan Anh <anhntth1807058@fpt.edu.vn>
2020-03-28 14:41:29 +0700

    notice

---
+++ Project_sem_3/Controllers/OrderNoticeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_sem_3.Controllers
+{
+    public class OrderNoticeController : Controller
+    {
+        // GET: OrderNotice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
